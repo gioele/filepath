bbf49657861fec2ef745d8e220ef80036e519a4f
Gioele Barabucci <gioele@svario.it>
2015-08-14 17:18:21 +0200

    Enable RSpec 3 should syntax and collection matchers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,3 @@
-# This is free software released into the public domain (CC0 license).
-
 LIB_DIR = File.expand_path(File.join(File.dirname(__FILE__), %w[.. lib]))
 $LOAD_PATH.unshift(LIB_DIR) unless $LOAD_PATH.include?(LIB_DIR)
 
@@ -9,4 +7,16 @@ require File.join(File.dirname(__FILE__), 'fixtures')
 
 RSpec.configure do |config|
 	config.filter_run_excluding :broken => true
+
+	config.expect_with :rspec do |rspec|
+		rspec.syntax = [:should, :expect]
+	end
+
+	config.mock_with :rspec do |mocks|
+		mocks.syntax = [:should, :expect]
+	end
 end
+
+require 'rspec/collection_matchers'
+
+# This is free software released into the public domain (CC0 license).
